737a902ec0018afb5c169608e0f6103558c6ba63
ovisnovis <93317057+ovisnovis@users.noreply.github.com>
2022-09-25 21:17:52 +0100

    maps approach

---
+++ src/GradesOnMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class GradesOnMap {
+    //alternate way by using maps
+    public void classAverageAnalyser() {
+        List<String> namesStudents = new ArrayList<>();
+        Map<String, Double> gradesStudents = new HashMap<>();
+
+    }
+}
