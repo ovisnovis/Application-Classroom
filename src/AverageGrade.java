7157ab3e213b8a36ef8a920300d616b2c26255d6
ovisnovis <andreas.bpdl@gmail.com>
2022-09-27 21:37:13 +0200

    new with students list

--- src/AverageGrade.java
+++ src/AverageGrade.java
@@ -7,15 +7,13 @@ import java.util.Scanner;
 
 public class AverageGrade {
     private static final DecimalFormat df = new DecimalFormat("0.00");
+    static List<Student> classroom = new ArrayList<>();
 
     public static List<String> inputFile() {
         List<String> dataList = new ArrayList<>();
         try {
-            File textFile = new File("grades-v01.txt");
-            Scanner scn = new Scanner(textFile);
-            while (scn.hasNextLine()) {
-                dataList.add(scn.nextLine());
-            }
+            Scanner scn = new Scanner(new File("grades-v01.txt"));
+            while (scn.hasNextLine()) dataList.add(scn.nextLine());
             scn.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
@@ -28,14 +26,19 @@ public class AverageGrade {
         for (String student : inputFile()) {
             String[] who = student.split(",");
             double counter = 0;
-            int nameCounter = 0;
             for (int i = 1; i < who.length; i++) {
                 counter = counter + Double.parseDouble(who[i]);
             }
-            System.out.print("The average grade for " + who[nameCounter] + " is: ");
-            System.out.println(df.format(counter / (who.length - 1)));
-            counter = 0;
-            nameCounter++;
+            Student kid = new Student(who[0], df.format(counter / (who.length - 1)));
+            classroom.add(kid);
+        }
+    }
+
+    public static void printStudents() {
+        dataSplit();
+        for (Student object : classroom
+        ) {
+            System.out.println("The average grade for " + object.name + " is: " + object.avgGrade);
         }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public String name;
+    public String avgGrade;
+
+    public Student(String name, String avgGrade) {
+        this.name = name;
+        this.avgGrade = avgGrade;
+    }
+}
